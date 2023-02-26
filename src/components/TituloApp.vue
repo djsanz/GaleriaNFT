5849f00336ad0ab275e50320d25a12960efb9718
Javier Sanz Calvo <djsanz@gmail.com>
2023-02-26 17:00:32 +0100

    Fix Logo Icon

--- src/components/TituloApp.vue
+++ src/components/TituloApp.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="flex flex-row align-middle items-center">
 		<div class="flex items-center basis-2/12">
-			<a href="https://mydynamicnft.com/" target="_blank">
-				<img src="@/assets/logos/logo.png" class="inline max-h-24" />
+			<a href="./">
+				<img src="@/assets/logos/logo.png" class="h-8 sm:h-16" />
 			</a>
 		</div>
 		<div class="basis-8/12 text-3xl sm:text-7xl font-bold font-Dancing text-amber-600 flex-nowrap">
@@ -10,7 +10,7 @@
 		</div>
 		<div class="flex justify-end basis-2/12">
 			<a href="https://mydynamicnft.com/" target="_blank">
-				<img src="@/assets/images/MintNFT2.png" class="inline max-h-28 -rotate-6 hover:scale-125" />
+				<img src="@/assets/images/MintNFT2.png" class="inline max-h-28 -rotate-6 hover:scale-125 hover:-rotate-12" />
 			</a>
 		</div>
 	</div>
